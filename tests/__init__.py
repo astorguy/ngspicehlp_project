9e16038c5829312b3e016125fa34685baae2658a
Mark Chadwick <mark.chadwick@techsharpen.com>
2023-03-03 14:30:38 -0800

    make __init__.py a blank file

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""__init__.py"""
-from .test_kicad import test_kicad_cmd_str
-from .test_vectors import test_vectors_one_signal_list
-
-
-__all__ = (
-    "test_kicad_cmd_str",
-    "test_vectors_one_signal_list",
-)
