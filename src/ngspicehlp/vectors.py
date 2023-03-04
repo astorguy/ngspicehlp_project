eb8bea5cb25a5305ee91a0d6f74b44f0bcc39ab1
Mark Chadwick <mark.chadwick@techsharpen.com>
2023-03-04 12:27:49 -0800

    fix class vectors and test_vectors

--- src/ngspicehlp/vectors.py
+++ src/ngspicehlp/vectors.py
@@ -9,66 +9,62 @@ T = TypeVar("T", bound="Vectors")
 class Vectors:
     """Vectors are a list of signals to specified for simulation, display, ..."""
 
-    def __init__(self, vector: list[str | int]):
-        self.vector: list[str] = [str(signal) for signal in vector]
-        self._words_to_items()
+    def __init__(self, vect_init: list[str | int] | str | int):
+        self.__vect = []
+        if isinstance(vect_init, list):
+            self.__vect = [str(signal) for signal in vect_init]
+            self._words_to_items()
+        if isinstance(vect_init, str):
+            self.__vect = vect_init.split()
+        if isinstance(vect_init, int):
+            self.__vect = [str(vect_init)]
         self._remove_dups()
 
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.__vect})"
+
     def __str__(self) -> str:
-        return " ".join(self.vector)
+        return " ".join(self.__vect)
 
     def _words_to_items(self) -> None:
         """Convert words separated by spaces into separate list items"""
         my_list = []
-        for item in self.vector:
+        for item in self.__vect:
             if " " in item:
                 # Split the string into multiple items
-                split_items = item.split(" ")
+                split_items = item.split()
                 # Add the new items to the new list
                 my_list.extend(split_items)
             else:
                 my_list.append(item)
-        self.vector = my_list
+        self.__vect = my_list
 
     def _remove_dups(self) -> None:
         """remove duplicate signals"""
-        my_list = self.vector
+        my_list = self.__vect
         no_dups: list[str] = []
         for item in my_list:
             if item not in no_dups:
                 no_dups.append(item)
-        self.vector = no_dups
+        self.__vect = no_dups
 
     def list_out(self) -> list[str]:
         """Outputs vector as a list"""
-        return self.vector
+        return self.__vect
 
     def sort(self) -> None:
         """Sort the signals"""
-        self.vector = sorted(self.vector)
-
-    def _union_one(self: T, vec2: T) -> None:
-        """Combine the vector object with a second one"""
-        self.vector = self.list_out() + vec2.list_out()
-        self._remove_dups()
+        self.__vect = sorted(self.__vect)
 
     def union(self: T, *vecs: T) -> None:
-        """Combine the vector ob
-        ject with others passed in"""
+        """Combine the vector object with others passed in"""
         for vec in vecs:
-            self._union_one(vec)
+            self.__vect = self.__vect + vec.list_out()
+            self._remove_dups()
 
     def subtract(self: T, vec2: T) -> None:
         """Remove from this vector the signals in the second one"""
         list1 = self.list_out()
         list2 = vec2.list_out()
-        self.vector = [item for item in list1 if item not in list2]
+        self.__vect = [item for item in list1 if item not in list2]
         self._remove_dups()
-
-
-def main() -> None:
-    """main"""
-
-
-if __name__ == "__main__":
-    main()
